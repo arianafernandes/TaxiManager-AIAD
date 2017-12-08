1f7a7e395e7c9920ec684e58e874f60c1fe3b1f2
Diogo Almeida Cunha <diogocunha588@gmail.com>
2017-12-08 11:14:22 +0100

    interface

--- TaxiM/src/MyLauncher.java
+++ TaxiM/src/MyLauncher.java
@@ -5,54 +5,144 @@ import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.StaleProxyException;
 import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.graph.NetworkBuilder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.parameter.Parameters;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import repast.simphony.space.grid.WrapAroundBorders;
 import sajas.core.Agent;
 import sajas.core.Runtime;
 import sajas.sim.repasts.RepastSLauncher;
 import sajas.wrapper.AgentController;
 import sajas.wrapper.ContainerController;
-
+import repast.simphony.context.Context;
+import repast.simphony.context.DefaultContext;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.graph.NetworkBuilder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.parameter.Parameters;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import repast.simphony.space.grid.WrapAroundBorders;
+ 
 public class MyLauncher extends RepastSLauncher {
-	
-	private ContainerController mainContainer;
-	
-	@Override
-	public String getName() {
-		return "SAJaS Project";
-	}
-
-	@Override
-	protected void launchJADE() {
-		
-		Runtime rt = Runtime.instance();
-		Profile p1 = new ProfileImpl();
-		mainContainer = rt.createMainContainer(p1);
-		
-		launchAgents();
-	}
-	
-	private void launchAgents() {
-		
-		try {
-			
-			Central central = new Central(1,1);			
-			mainContainer.acceptNewAgent("[CENTRAL] ", central).start();
-			Client client = new Client(1);			
-			mainContainer.acceptNewAgent("[CLIENT] ", client).start();
-			Taxi taxi = new Taxi(1,2);			
-			mainContainer.acceptNewAgent("[Taxi] ", taxi).start();
-
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	@Override
-	public Context build(Context<Object> context) {
-		
-		// ...
-		
-		return super.build(context);
-	}
-
+ 
+    public ContainerController mainContainer;
+    public ContinuousSpaceFactory spaceFactory;
+    public GridFactory gridFactory;
+    public ContinuousSpace<Object> space;
+    public Grid<Object> grid;
+    public Context<Object> context;
+   
+    @Override
+    public String getName() {
+        return "taximanager";
+    }
+ 
+    @Override
+    protected void launchJADE() {
+ 
+        Runtime rt = Runtime.instance();
+        Profile p1 = new ProfileImpl();
+        mainContainer = rt.createMainContainer(p1);
+ 
+        try {
+            launchAgents();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+ 
+    private void launchAgents() throws InterruptedException {
+       
+        Parameters params = RunEnvironment.getInstance().getParameters();
+ 
+        try {
+            Central central = new Central(1, 1);
+            mainContainer.acceptNewAgent("[CENTRAL]", central).start();
+ 
+            int taxiCount = (Integer) params.getValue("number_taxis");
+            for (int i = 0; i < taxiCount; i++) {
+                Taxi taxi = new Taxi(space, grid, 1, 4);
+                context.add(taxi);
+                mainContainer.acceptNewAgent("[Taxi " + i + "]", taxi).start();
+ 
+            }
+ 
+            int clientCount = (Integer) params.getValue("number_clients");
+            for (int i = 0; i < clientCount; i++) {
+                Client client = new Client(space, grid, clientCount);
+                context.add(client);
+                mainContainer.acceptNewAgent("[CLIENT " + i + "]", client)
+                        .start();
+ 
+            }
+        } catch (StaleProxyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * repast.simphony.dataLoader.ContextBuilder#build(repast.simphony.context
+     * .Context)
+     */
+    @Override
+    public Context build(Context<Object> context) {
+        context.setId("taximanager");
+ 
+        // NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>(
+        // "aldeia", context, true);
+        // netBuilder.buildNetwork();
+ 
+        spaceFactory = ContinuousSpaceFactoryFinder
+                .createContinuousSpaceFactory(null);
+        space = spaceFactory.createContinuousSpace(
+                "space", context, new RandomCartesianAdder<Object>(),
+                new repast.simphony.space.continuous.WrapAroundBorders(), 50,
+                50);
+ 
+        gridFactory = GridFactoryFinder.createGridFactory(null);
+        grid = gridFactory.createGrid("grid", context,
+                new GridBuilderParameters<Object>(new WrapAroundBorders(),
+                        new SimpleGridAdder<Object>(), false, 50, 50));
+ 
+        for (Object obj : context) {
+            NdPoint pt = space.getLocation(obj);
+            grid.moveTo(obj, (int) pt.getX(), (int) pt.getY());
+        }
+ 
+        if (RunEnvironment.getInstance().isBatch()) {
+            RunEnvironment.getInstance().endAt(20);
+        }
+       
+        this.context = context;
+       
+        return super.build(context);
+    }
+ 
 }
--- TaxiM/src/agents/Client.java
+++ TaxiM/src/agents/Client.java
@@ -1,7 +1,9 @@
 package agents;
-
+ 
 import java.util.Random;
-
+ 
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
 import sajas.core.Agent;
 import sajas.core.behaviours.OneShotBehaviour;
 import sajas.core.behaviours.SimpleBehaviour;
@@ -10,125 +12,124 @@ import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
-
+ 
 @SuppressWarnings("serial")
 public class Client extends Agent {
-
-	int nClientes;
-	int xi = 0;
-	int xf;
-	int yi;
-	int yf;
-
-	public Client() {
-	}
-	public Client(int nClientes) {
-		this.nClientes = nClientes;
-	}
-	//inicia todas as variaveis  do cliente
-	private void setClientProperties() {
-		Random r = new Random();
-		this.xi = Math.abs(r.nextInt()) % 20;
-		this.yi = Math.abs(r.nextInt()) % 20;
-		this.xf = Math.abs(r.nextInt()) % 20;
-		this.yf = Math.abs(r.nextInt()) % 20;
-		this.nClientes = (Math.abs(r.nextInt()) % 4 + 1);
-
-	}
-
-	// client behaviour é one shot behaviour pois o agent so tem um
-	// comportamento que é pedir o taxi
-	class ClientBehaviour extends SimpleBehaviour {
-		// construtor do behaviour
-		Agent myAgent;
-
-		ACLMessage m = null;
-		public ClientBehaviour(Agent a) {
-			super(a);
-			myAgent = a;
-			DFAgentDescription template = new DFAgentDescription();
-			ServiceDescription sd1 = new ServiceDescription();
-			sd1.setType("Central");
-			template.addServices(sd1);
-			try {
-				// procura todos os agentes no df service
-				DFAgentDescription[] result = DFService.search(a, template);
-				// envia uma mensagem do tipo request para a central
-				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
-				for (int i = 0; i < result.length; ++i)
-					msg.addReceiver(result[i].getName());
-
-				//Cliente pede taxi para nClientes pessoas
-				String nC = Integer.toString(nClientes);
-				String xiS = Integer.toString(xi);
-				String yiS = Integer.toString(yi);
-				String xfS = Integer.toString(xf);
-				String yfS = Integer.toString(yf);
-				msg.setContent(nC + "," + xiS + "," + yiS + "," + xfS + "," + yfS);
-
-				System.out.println("---------------------------------");
-				System.out.println(a.getLocalName() + ": Quero um taxi para " + nClientes + " pessoa(s) do sitio " + 
-						xi + "-" + yi + " para o sitio " + xf + "-" + yf + ".");
-				send(msg);
-			} catch (FIPAException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// método action
-		public void action() {
-
-			ACLMessage msg = receive();
-
-			if(msg != null){
-				//se recebe mensagem inform da central
-				if (msg.getPerformative() == ACLMessage.INFORM){
-					System.out.println(myAgent.getLocalName() + ": Obrigado CENTRAL, fico à espera do " + msg.getContent() +".");
-				} 
-				if(msg.getPerformative() == ACLMessage.REFUSE){
-					System.out.println(myAgent.getLocalName() + ": OK. Dentro de minutos faço novo pedido.");
-				}
-
-				if(msg.getPerformative() == ACLMessage.FAILURE){
-					System.out.println("FAILURE");
-				}
-
-			}
-			else{
-				block();
-			}
-		}
-
-		@Override
-		public boolean done() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-	}
-
-	protected void setup() {
-		//		Object[] args = getArguments();
-		//		if (args != null) {
-		//			// Extracting the integer.
-		//			//this.nClientes = Integer.parseInt((String) args[0]);
-		//		}
-		setClientProperties();
-		// regista agente no DF
-		DFAgentDescription dfd = new DFAgentDescription();
-		dfd.setName(getAID());
-		// descreve servico
-		ServiceDescription sd = new ServiceDescription();
-		sd.setName(getName());
-		sd.setType("Client");
-		dfd.addServices(sd);
-		try {
-			DFService.register(this, dfd);
-		} catch (FIPAException e) {
-			e.printStackTrace();
-		}
-		ClientBehaviour b = new ClientBehaviour(this);
-		addBehaviour(b);
-
-	}
-
+ 
+    int nClientes;
+    int xi = 0;
+    int xf;
+    int yi;
+    int yf;
+    ContinuousSpace<Object> space;
+    Grid<Object> grid;
+   
+    public Client() {
+    }
+    public Client(ContinuousSpace<Object> space, Grid<Object> grid, int nClientes) {
+        this.nClientes = nClientes;
+        Random r = new Random();
+        this.xi = Math.abs(r.nextInt()) % 20;
+        this.yi = Math.abs(r.nextInt()) % 20;
+        this.xf = Math.abs(r.nextInt()) % 20;
+        this.yf = Math.abs(r.nextInt()) % 20;
+        this.nClientes = (Math.abs(r.nextInt()) % 4 + 1);
+        this.space = space;
+        this.grid = grid;
+    }
+ 
+    // client behaviour é one shot behaviour pois o agent so tem um
+    // comportamento que é pedir o taxi
+    class ClientBehaviour extends SimpleBehaviour {
+        // construtor do behaviour
+        Agent myAgent;
+ 
+        ACLMessage m = null;
+        public ClientBehaviour(Agent a) {
+            super(a);
+            myAgent = a;
+            DFAgentDescription template = new DFAgentDescription();
+            ServiceDescription sd1 = new ServiceDescription();
+            sd1.setType("Central");
+            template.addServices(sd1);
+            try {
+                // procura todos os agentes no df service
+                DFAgentDescription[] result = DFService.search(a, template);
+                // envia uma mensagem do tipo request para a central
+                ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+                for (int i = 0; i < result.length; ++i)
+                    msg.addReceiver(result[i].getName());
+ 
+                //Cliente pede taxi para nClientes pessoas
+                String nC = Integer.toString(nClientes);
+                String xiS = Integer.toString(xi);
+                String yiS = Integer.toString(yi);
+                String xfS = Integer.toString(xf);
+                String yfS = Integer.toString(yf);
+                msg.setContent(nC + "," + xiS + "," + yiS + "," + xfS + "," + yfS);
+ 
+                System.out.println("---------------------------------");
+                System.out.println(a.getLocalName() + ": Quero um taxi para " + nClientes + " pessoa(s) do sitio " +
+                        xi + "-" + yi + " para o sitio " + xf + "-" + yf + ".");
+                send(msg);
+            } catch (FIPAException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        // método action
+        public void action() {
+ 
+            ACLMessage msg = receive();
+ 
+            if(msg != null){
+                //se recebe mensagem inform da central
+                if (msg.getPerformative() == ACLMessage.INFORM){
+                    System.out.println(myAgent.getLocalName() + ": Obrigado CENTRAL, fico à espera do " + msg.getContent() +".");
+                }
+                if(msg.getPerformative() == ACLMessage.REFUSE){
+                    System.out.println(myAgent.getLocalName() + ": OK. Dentro de minutos faço novo pedido.");
+                }
+ 
+                if(msg.getPerformative() == ACLMessage.FAILURE){
+                    System.out.println("FAILURE");
+                }
+ 
+            }
+            else{
+                block();
+            }
+        }
+ 
+        @Override
+        public boolean done() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+    }
+ 
+    protected void setup() {
+        //      Object[] args = getArguments();
+        //      if (args != null) {
+        //          // Extracting the integer.
+        //          //this.nClientes = Integer.parseInt((String) args[0]);
+        //      }
+        // regista agente no DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        // descreve servico
+        ServiceDescription sd = new ServiceDescription();
+        sd.setName(getName());
+        sd.setType("Client");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+        ClientBehaviour b = new ClientBehaviour(this);
+        addBehaviour(b);
+ 
+    }
+ 
 }
